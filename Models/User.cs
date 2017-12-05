3e8aa9d4a36bdd72a602cf84af503e37f21f0182
Alex Roitman <alexr_lu@lagash.com>
2017-12-05 09:43:21 -0300

    Metodo Add

--- Models/User.cs
+++ Models/User.cs
@@ -1,26 +1,39 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Http;
 
 namespace mejor_precio_3.Models
 {
     public class User
     {
-        public string name { get; set; }
-        public string mail { get; set; }
-         [Required]
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        [Required]
         [DataType(DataType.Password)]
-        public string password { get; set; }
-        public int age { get; set; }
-        public string gender { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        Queue queue;
+
+        public string Add(User user)
+        {
+            var pass = user.Password;
+            Regex pat = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+
+            if (!pat.IsMatch(user.Password))
+                return "Error en la contrasena +8, Mayus,Minus,numero";
+            Regex regex = new Regex(@"(\w+)@(\w+)\.(\w+)");
+            if (!regex.IsMatch(user.Mail))
+                return "Error en el mail";
+
+            return "OK";
 
 
-        public HttpStatusCode Create (User user)
-        {            
 
-            return HttpStatusCode.OK;
-            
         }
     }
 }
