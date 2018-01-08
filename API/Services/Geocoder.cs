d8e07cab6332e077f99ae409864f4bb843cac703
Juan Pablo Vallejo <juanpv_lu@lagash.com>
2018-01-08 10:53:57 -0300

    cambios para no repetir codigo sin sentido

--- API/Services/Geocoder.cs
+++ API/Services/Geocoder.cs
@@ -9,9 +9,9 @@ namespace MejorPrecio3.Services
     public class Geocoder
     {
 
+        private IGeocoder geocoder = new GoogleGeocoder() { ApiKey = "AIzaSyCjP-q3fTsUk-z0pPGVlnmW_LilMGX4k6c" };
         public Tuple<double, double> GetLatLong(string adress)
         {
-            IGeocoder geocoder = new GoogleGeocoder() { ApiKey = "AIzaSyCjP-q3fTsUk-z0pPGVlnmW_LilMGX4k6c" };
             IEnumerable<Address> addresses = geocoder.GeocodeAsync(adress + "Buenos Aires Argentina").Result;
             var tuple = new Tuple<double, double>(addresses.First().Coordinates.Latitude, addresses.First().Coordinates.Longitude);
             return tuple;
@@ -19,7 +19,6 @@ namespace MejorPrecio3.Services
 
         public string GetAdress(double latitude, double longitude)
         {
-            IGeocoder geocoder = new GoogleGeocoder() { ApiKey = "AIzaSyCjP-q3fTsUk-z0pPGVlnmW_LilMGX4k6c" };
             IEnumerable<Address> addresses = geocoder.ReverseGeocodeAsync(latitude,longitude).Result;
             return addresses.First().FormattedAddress;
         }
