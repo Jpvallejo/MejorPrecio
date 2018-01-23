8fcfde03ea65300820e9d75ba30a7b9f71a6478b
Juan Pablo Vallejo <juanpv_lu@lagash.com>
2018-01-23 11:56:58 -0300

    Agregado vistas de administrador para eliminar precios y productos. Agregado relleno automatico de codigo de barras al crear prod

--- MVC/Models/AccountIndexViewModel.cs
+++ MVC/Models/AccountIndexViewModel.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
+using MejorPrecio3.Models;
 
 namespace MejorPrecio3.MVC.Models
 {
     public class AccountIndexViewModel
     {
-        public List<HistoryViewModel> history { get; set; }
+        public List<History> history { get; set; }
 
         public string name { get; set; }
 
---
+++ MVC/wwwroot/js/barcodeScript.js
@@ -0,0 +1,35 @@
+
+function GetBarcode()
+    {
+      var datas = new FormData();
+    var files = $("#file").get(0).files;
+    if (files.length > 0) {
+        datas.append("HelpSectionImages", files[0]);
+    }
+      var file_data = $("#file").prop("files")[0];   // Getting the properties of file from file field
+	    var form_data = new FormData();                  // Creating object of FormData class
+	    form_data.append("file", file_data)
+        var data = function(){
+            var tmp = null;
+            $.ajax({
+            type: "POST",
+            data: form_data,
+            async: false,
+            url:  'http://localhost:5000/Products/GetBarcode',
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (result)
+            {
+            
+                tmp = result;
+            }
+            });
+            return tmp;
+        }();
+
+        $("#barcode").val(data);  
+
+
+
+    }
